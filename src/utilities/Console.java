2da046f4a107a1bfcec6b6285cc7af13fa2a7362
Matthew Greco <mgreco2112@tutanota.com>
2021-10-23 12:39:46 -0400

    initial commit, created Console class and methods

---
+++ src/utilities/Console.java
@@ -0,0 +1,44 @@
+package utilities;
+
+import java.util.Scanner;
+
+public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int getInt(String prompt, int min, int max) {
+        int output = min - 1;
+
+        do {
+            System.out.println(prompt);
+
+            String input = scanner.nextLine();
+
+            try {
+                output = Integer.parseInt(input);
+            } catch (Exception e) {
+                System.out.println("Please enter a Number");
+                output = min - 1;
+            }
+
+        }  while (output <= min || output >= max);
+
+        return output;
+    }
+
+    public static String getString(String prompt, boolean isRequired) {
+        String output = "";
+
+
+        System.out.println(prompt);
+
+        output = scanner.nextLine();
+
+        if (output.equals("") && isRequired) {
+            System.out.println("Input is required");
+            getString(prompt, isRequired);
+        }
+
+        return output;
+    }
+
+}
